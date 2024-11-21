4d1aa84c12ca49a46201fc48bdd16cade935a793
webdevsaravanan <webdevsaravanan@gmail.com>
2024-11-21 23:36:39 +0530

    Form Concepts Day-2

--- my-app/src/Form.js
+++ my-app/src/Form.js
@@ -1,45 +1,61 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import React, { useState } from "react";
+import { useState } from "react";
 const Form = () => {
-const [username,setUsername] = useState('')
-const [password,setPassword] = useState('')
+  const [status, setStatus] = useState(null);
+  const[data,setData] = useState({
+    username: "",
+    password:""
+  });
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    if (!data.username || !data.password) {
+      setStatus(false);
+    } else {
+      setStatus(true);
+    }
+  };
+ const handleChange =(event) =>{
+     let { name, value } = event.target;
+    setData({ ...data, [name]: value })
+    console.log(data);
+ }
  return (
-  <>
-   <div className="container">
-    <form>
-     <div className="form-group">
-      <label htmlFor="name">Username:</label>
-      <input
-       style={{ width: "40%" }}
-       type="text"
-       className="form-control"
-       id="name"
-       placeholder="Enter Name"
-       name="name"
-       value={username}
-      onChange={(event) => setUsername(event.target.value)}
-      />
-     </div>
-     <div className="form-group">
-      <label htmlFor="pwd">Password:</label>
-      <input
-       style={{ width: "40%" }}
-       type="password"
-       className="form-control"
-       id="pwd"
-       placeholder="Enter password"
-       name="password"
-       value={password}
-       onChange={(event) => setPassword(event.target.value)}
-      />
-     </div>
-     <button type="button" className="btn btn-primary">
-      Login
-     </button>
-    </form>
-   </div>
-   <h4>Welcome {username} !!</h4>
-  </>
- );
+    <>
+      <div className="container">
+        <form onSubmit={handleSubmit}>
+          <div className="form-group">
+            <label htmlFor="name">Username:</label>
+            <input
+              style={{ width: "40%" }}
+              type="text"
+              id="username"
+              name = "username"
+              value={data.username}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Enter Name"
+            />
+          </div>
+          <div className="form-group">
+            <label htmlFor="pwd">Password:</label>
+            <input
+              style={{ width: "40%" }}
+              type="password"
+              id="password"
+              name = "password"
+              value={data.password}
+              onChange={handleChange}
+              className="form-control"
+              placeholder="Enter password"
+            />
+          </div>
+          <button type="submit" className="btn btn-primary">
+            Login
+          </button>
+          {status === false && <div className="text-danger">Enter User Name and Password</div>}
+          {status === true && <div className="text-success">Login Successful</div>}
+        </form>
+      </div>
+    </>
+  );
 };
 export default Form;
